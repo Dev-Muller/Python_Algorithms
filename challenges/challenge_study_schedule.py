86efc844b6d25a8c75b4ba5c2c3f03e98937d08b
muller <pedromullergl11@gmail.com>
2023-09-20 18:18:51 -0300

    feat: logic study schedule implemented

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    counter = 0
+    try:
+        for start_time, end_time in permanence_period:
+            if start_time <= target_time <= end_time:
+                counter += 1
+        return counter
+    except TypeError:
+        return None
