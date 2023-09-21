5d71c74f24a276d44eb91f210de6f4afef5abdb9
muller <pedromullergl11@gmail.com>
2023-09-21 19:32:18 -0300

    feat: find duplicate logic implemented

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,20 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not isinstance(nums, list) or len(nums) < 2:
+        return False
+
+    nums.sort()
+
+    for i in range(1, len(nums)):
+        if not isinstance(nums[i], int) or nums[i] < 0:
+            return False
+        if nums[i] == nums[i - 1]:
+            return nums[i]
+
+    # teste de outra forma
+    # for i in nums:
+    #     if not isinstance(i, int) or i < 0:
+    #         return False
+    #     if i == nums[i - 1]:
+    #         return i
+
+    return False
